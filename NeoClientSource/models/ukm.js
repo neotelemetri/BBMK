c6a9b99e85571c7232474dfa29a05b15baa235d5
luvlamerz <ardall.leo@gmail.com>
2014-03-01 14:30:36 +0700

    Uploaded sample

---
+++ NeoClientSource/models/ukm.js
@@ -0,0 +1,6 @@
+﻿bbmk.ukm = function (obj) {
+    this.ukm_id = obj.ukm_id;
+    this.ukm_name = obj.ukm_id;
+    this.ukm_desc = obj.ukm_id;
+    this.ukm_status = obj.ukm_status;
+};
---
+++ NeoClientSource/ui/ui.js
@@ -0,0 +1,20 @@
+﻿bbmk.ui.ready = function () {
+
+    window.controllerUKM = new bbmk.controllerUKM();
+	controllerUKM = window.controllerUKM;
+	
+    bbmk.ui.rivets.bind();
+    bbmk.ui.rivets.setup();
+    bbmk.ui.bind();
+	
+	controllerUKM.setUKMList("{}",view);
+}
+
+/* Function to bind the element with handler */
+bbmk.ui.bind = function () {
+
+    $("#test").bind("click", function(){
+        $("#test-click").show();
+    });    
+
+}
